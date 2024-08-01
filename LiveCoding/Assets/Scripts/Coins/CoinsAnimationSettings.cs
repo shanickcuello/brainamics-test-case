439dc3a50c1db2cc6b7ec068eebd795d1ee831ba
shanickcuello <shanlogauthier@gmail.com>
2024-08-01 21:21:17 +0200

    Add CoinsAnimationSettings.cs files

---
+++ LiveCoding/Assets/Scripts/Coins/CoinsAnimationSettings.cs
@@ -0,0 +1,18 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Coins
+{
+    [CreateAssetMenu(fileName = "AnimationSettings", menuName = "Coin/AnimationSettings", order = 0)]
+    public class CoinsAnimationSettings : ScriptableObject
+    {
+        [SerializeField] public Vector3 rotationVector;
+        [SerializeField] public float rotationSpeed;
+        [SerializeField] public RotateMode rotateMode;
+        [SerializeField] public bool relative;
+        [SerializeField] public Ease easeAnimationCurve;
+        [SerializeField] public float duration;
+        [Header("Set -1 for infinit loop")]
+        public int loopsAmount;
+    }
+}
