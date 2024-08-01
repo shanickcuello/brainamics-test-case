2d3a44bcdb766de6e51255b7f67a8ef6d9156393
shanickcuello <shanlogauthier@gmail.com>
2024-08-01 20:22:33 +0200

    Instantiate Coin from Pooling

--- LiveCoding/Assets/Scripts/Coins/Coin.cs
+++ LiveCoding/Assets/Scripts/Coins/Coin.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Zenject;
@@ -7,17 +8,29 @@ namespace Coins
     public class Coin : MonoBehaviour, IPointerClickHandler
     {
         private SignalBus _signalBus;
+        private CoinsSpawner _coinsSpawner;
 
         [Inject]
-        public void Construct(SignalBus signalBus)
+        public void Construct(SignalBus signalBus, CoinsSpawner coinsSpawner)
         {
             _signalBus = signalBus;
+            _coinsSpawner = coinsSpawner;
         }
-    
+
         public void OnPointerClick(PointerEventData eventData)
         {
             _signalBus.Fire(new CoinCollectedSignal());
-            Destroy(gameObject);
+            _coinsSpawner.ReturnCoinToPool(this);
+        }
+
+        public static void TurnOn(Coin coin)
+        {
+            coin.gameObject.SetActive(true);
+        }
+
+        public static void TurnOff(Coin coin)
+        {
+            coin.gameObject.SetActive(false);
         }
     }
 }
--- LiveCoding/Assets/Scripts/Coins/CoinsSpawner.cs
+++ LiveCoding/Assets/Scripts/Coins/CoinsSpawner.cs
@@ -1,29 +1,43 @@
+using Pooling;
 using UnityEngine;
 
 namespace Coins
 {
-    public class Spawner : MonoBehaviour
+    public class CoinsSpawner : MonoBehaviour
     {
         public int InitialCount = 10;
+        public ObjectPool<Coin> Pool;
 
         [SerializeField] private Collider _bounds;
 
-        [SerializeField] private GameObject _coinPrefab;
+        [SerializeField] private Coin _coinPrefab;
 
         [SerializeField] private Transform _coinsParent;
-
+        
+        
         private void Start()
         {
+            Pool = new ObjectPool<Coin>(GetCoinInstance, Coin.TurnOn, Coin.TurnOff, InitialCount);
+
             for (var i = 0; i < InitialCount; i++)
-                Spawn();
+            {
+                var coin = Pool.GetObject();
+                var min = _bounds.bounds.min;
+                var max = _bounds.bounds.max;
+                var val = new Vector3(Random.Range(min.x, max.x), 0, Random.Range(min.z, max.z));
+                coin.transform.position = val;
+            }
+        }
+        
+        private Coin GetCoinInstance()
+        {
+            var coin = Instantiate(_coinPrefab, _coinsParent);
+            return coin;
         }
 
-        private void Spawn()
+        public void ReturnCoinToPool(Coin coin)
         {
-            var min = _bounds.bounds.min;
-            var max = _bounds.bounds.max;
-            var val = new Vector3(Random.Range(min.x, max.x), 0, Random.Range(min.z, max.z));
-            Instantiate(_coinPrefab, val, Quaternion.identity, _coinsParent);
+            Pool.ReturnObject(coin);
         }
     }
 }
